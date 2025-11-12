439a909b2abf2a095e58cf05c72610bb951a1c01
marlonrobledo-coder <marlonrobledo@gmail.com>
2025-11-11 21:52:28 -0300

    Update base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   // Adicione esta linha com o nome do seu repositório
-  base: '/NOME_DO_SEU_REPOSITORIO/',
+  base: '/HNET/',
   plugins: [react()],
 });
       plugins: [react()],
